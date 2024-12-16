8031468586e7bb1b10cdbded73ecfe499d7ce0db
AgustinAlbonico <agustin_010@live.com>
2024-12-16 16:56:38 -0300

    tp listo para presentar

--- src/controllers/schemaValidations/user.schema.ts
+++ src/controllers/schemaValidations/user.schema.ts
@@ -3,7 +3,7 @@ import { IUser } from "../../interfaces";
 
 const registerSchema = Joi.object<IUser>({
   email: Joi.string().email().required(),
-  password: Joi.string().min(6).required(),
+  password: Joi.string().min(5).required(),
   name: Joi.string().min(2).required(),
   lastname: Joi.string().min(2).required(),
   birthdate: Joi.date().less("now").required(),
